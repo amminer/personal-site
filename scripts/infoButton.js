b59be3d918675bf151fecc59e43a851ffeb4723f
Amelia Miner <ameliamminer@gmail.com>
2022-08-04 11:01:04 -0700

    Not sure why these weren't included with last commit...

---
+++ scripts/infoButton.js
@@ -0,0 +1,5 @@
+$(document.getElementById('info-btn')).click((event) => {
+  window.alert(
+    "This site was created for Portland State University's Intro to Web Development course, using raw HTML5 CSS and JavaScript plus Bootstrap 4 - watch this space!"
+  );
+});
