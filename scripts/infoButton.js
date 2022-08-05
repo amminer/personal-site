09c4b93d4b50d2745fb97acf4b3a98d2329ffa49
Amelia Miner <ameliamminer@gmail.com>
2022-08-04 18:50:02 -0700

    Minor adjustments to text and scrollbars

--- scripts/infoButton.js
+++ scripts/infoButton.js
@@ -1,5 +1,5 @@
 $(document.getElementById('info-btn')).click((event) => {
   window.alert(
-    "This site was created for Portland State University's Intro to Web Development course, using raw HTML5 CSS and JavaScript plus Bootstrap 4 - watch this space!"
+    "Hello dear visitor,\nThis site was created for Portland State University's Intro to Web Development course, using raw HTML5 CSS and JavaScript plus Bootstrap 4 (and, by necessity, JQuery) - however I have plans to keep building this out as my career comes along. Watch this space!\n-Amelia"
   );
 });
