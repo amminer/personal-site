d70625d725fe8bb6c0f7777b673f6e3f4f310724
Amelia Miner <ameliamminer@gmail.com>
2022-08-04 23:00:39 -0700

    Added another project card for asciifier, Changed some more text

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,1 +1,24 @@
-//TODO
+// Initialize with public key
+(() => {
+  emailjs.init('idTbU1MrUkQBudGaA');
+})();
+
+// Set up event listener for submission
+window.onload = () => {
+  document
+    .getElementById('contact-form')
+    .addEventListener('submit', (event) => {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this).then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED: ', error);
+        }
+      );
+    });
+};
