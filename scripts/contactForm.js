a01c1ba31b763bfe5b70c1eee3ed54ab787521a1
Amelia Miner <ameliamminer@gmail.com>
2022-08-08 11:31:47 -0700

    Finished implementing form... whoops!

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -3,18 +3,45 @@
   // https://dashboard.emailjs.com/admin/account
   return emailjs.init('idTbU1MrUkQBudGaA');
 })();
+
+// Initialize API
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  return emailjs.init('idTbU1MrUkQBudGaA');
+})();
+
+function formError(error) {
+  console.log('FAILED...', error);
+  window.alert('Something went wrong, please try again.');
+}
+
+function formSuccess() {
+  console.log('SUCCESS!');
+  window.alert('Your message was sent!');
+}
+
+function formValidate() {
+  let name = document.getElementById('name');
+  let email = document.getElementById('email');
+  let message = document.getElementById('message');
+  if (name.value === '' || email.value === '' || message.value === '') {
+    window.alert('Message could not be sent - please fill all fields.');
+    return false;
+  }
+  return true;
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  if (formValidate()) {
+    emailjs
+      .sendForm('personal-site', 'template_x62c35i', this)
+      .then(formSuccess, formError);
+  }
+}
+
 window.onload = function () {
   document
     .getElementById('contact-form')
-    .addEventListener('submit', function (event) {
-      event.preventDefault();
-      emailjs.sendForm('personal-site', 'template_x62c35i', this).then(
-        function () {
-          console.log('SUCCESS!');
-        },
-        function (error) {
-          console.log('FAILED...', error);
-        }
-      );
-    });
+    .addEventListener('submit', formSubmit);
 };
